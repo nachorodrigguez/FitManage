3de60852d12cf0ce1f923b45baf341f3ff3b1612
Ignacio Rodriguez <132673620+nachorodrigguez@users.noreply.github.com>
2025-10-27 19:05:11 +0000

    PERSONA.CPP V1

--- persona.cpp
+++ persona.cpp
@@ -1,31 +1,79 @@
-#pragma once
-#include <string>
+#include <iostream>
+#include <cstring>
+#include "persona.h"
 
-#include "Fecha.h"
+using namespace std;
 
-class Persona{
-    private:
-    char nombre[50];
-    char apellido[50];
-    char domicilio[50];
-    char telefono[15];
-    Fecha fechaNacimiento;
+Persona::Persona(){
+    strcpy(nombre, "S/N");
+    strcpy(apellido, "S/A");
+    strcpy(domicilio, "S/D");
+    strcpy(telefono, "00 0000 0000");
+}
 
-    public:
-    Persona();
+void Persona::setNombre(const char *n){
+    strcpy(nombre, n);
+}
 
-    const char* getNombre();
-    const char* getApellido();
-    const char* getDomicilio();
-    const char* getTelefono();
-    Fecha getFechaNacimiento();
+void Persona::setApellido(const char *n){
+    strcpy(apellido, n);
+}
 
-    void setNombre(const char *);
-    void setApellido(const char *);
-    void setDomicilio(const char *);
-    void setTelefono(const char *);
-    void setFechaNacimiento(Fecha);
+void Persona::setDomicilio(const char *n){
+    strcpy(domicilio, n);
+}
 
-    void Cargar();
-    void Mostrar();
-} ;
+void Persona::setTelefono(const char *n){
+    strcpy(telefono, n);
+}
+
+void Persona::setFechaNacimiento(Fecha fN){
+    fechaNacimiento= fN;
+}
+
+const char* Persona::getNombre(){
+    return nombre;
+}
+
+const char* Persona::getApellido(){
+    return apellido;
+}
+
+const char* Persona::getDomicilio(){
+    return domicilio;
+}
+
+const char* Persona::getTelefono(){
+    return telefono;
+}
+
+Fecha Persona::getFechaNacimiento(){
+    return fechaNacimiento;
+}
+
+void Persona::Cargar(){
+    cin.ignore();
+    cout << "Ingrese nombre: ";
+    cin.getline(nombre, 50);
+
+    cout << "Ingrese apellido: ";
+    cin.getline(apellido,50);
+
+    cout << "Ingrese domicilio: ";
+    cin.getline(domicilio, 50);
+
+    cout << "Ingrese telefono: " ;
+    cin.getline(telefono,15);
+
+    cout <<"Fecha de nacimiento " << endl ;
+    fechaNacimiento.Cargar();
+}
+
+void Persona::Mostrar(){
+    cout << "Nombre: " << nombre << endl;
+    cout << "Apellido: " << apellido << endl;
+    cout << "Domicilio: " << domicilio << endl;
+    cout << "Telefono: " << telefono << endl;
+    cout << "Fecha de nacimiento: " << endl;
+    fechaNacimiento.Mostrar();
+}
