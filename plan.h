71dafb24ce736ac67300b24580224e6342b578ce
santeotero <santeotero22@gmail.com>
2025-10-28 20:43:23 -0300

    Update plan.h

--- plan.h
+++ plan.h
@@ -4,24 +4,29 @@
 class Plan{
     private:
     int idPlan;
-    float precio;
+    char tipoPlan[50];         // Flex, Plus o Total
     char descripcion[50];
-    char tipoPlan[50];
+    char periodo[20];          // Mensual o Anual
+    float precio;
+    int duracionMeses;
 
     public:
-    Plan();
+    Plan(int id = 0, const char* tip = "", const char* per = "", float pre = 0.0, int dur = 0);
+    
+    void setTipoPlan(const char * tip);    
+    void setPeriodo(const char* per);
+    void setPrecio(float pre);
+    void setDuracion(int dur);
 
     int getIdPlan();
-    float getPrecio();
-    const char* getDescripcion();
-    const char* getTipoPlan();
-
-    void setIdPlan(int);
-    void setPrecio(float);
-    void setDescripcion (const char *);
-    void setTipoPlan(const char *);
+    const char* getTipo() const;
+    const char* getDescripcion() const;
+    const char* getPeriodo() const;
+    float getPrecio() const;
+    int getDuracion() const;
 
     void Cargar();
     void Mostrar();
 
 } ;
+
