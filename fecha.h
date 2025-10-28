7fb8f2dcae05b1ab19ca0d76daf6b8d720ec498a
santeotero <santeotero22@gmail.com>
2025-10-28 20:09:58 -0300

    Update fecha.h

--- fecha.h
+++ fecha.h
@@ -8,16 +8,18 @@ class Fecha{
         int _anio;
 
     public:
-        Fecha(int dia=1, int mes=1, int anio=2000);
-
-        int getDia();
-        int getMes();
-        int getAnio();
+        Fecha(int dia=1, int mes=1, int anio=1900);
 
         void setDia(int);
         void setMes(int);
         void setAnio(int);
 
+        int getDia();
+        int getMes();
+        int getAnio();
+
         void Cargar();
         void Mostrar();
 };
+
+#endif 
