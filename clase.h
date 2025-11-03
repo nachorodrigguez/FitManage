261cd6c24b8ce1e41cc2e29f51537ffa9947d188
IgnaQuiroA <agustin.qp@hotmail.com.ar>
2025-11-03 08:14:37 -0300

    Update clase.h
    agregue capacidad y duracion de la clases

--- clase.h
+++ clase.h
@@ -5,17 +5,24 @@ class Clase{
     private:
     char nombreClase[50];
     char descripcion[100];
+    int tiempoDuracion; // en minutos
+    int capacidadMax; // cantidad de alumnos permitida
 
     public:
     Clase();
 
     const char* getNombreClase();
     const char* getDescripcion();
+    int getTiempoDuracion();
+    int getCapacidadMax();
 
     void setNombreclase(const char *);
     void setDescripcion (const char *);
+    void setTiempoDuracion(int);
+    void setCapacidadMax(int);
 
     void Cargar();
     void Mostrar();
 
 } ;
+
