8dd5201c2bcc18c980b87971e6bbe75f1edb52a2
Ignacio Rodriguez <132673620+nachorodrigguez@users.noreply.github.com>
2025-11-04 15:34:08 -0300

    Manager Planes

---
+++ managerPlanes.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "managerPlanes.h"
+
+using namespace std;
+
+ManagerPlanes::ManagerPlanes(){
+    _cantidadOpcines = 3 ;
+}
+
+void ManagerPlanes::run(){
+    int opcion;
+    do{
+        system("cls");
+        opcion = seleccionOpcion();
+        system("cls");
+        ejecutarOpcion(opcion);
+    }while(opcion!=0);
+}
+
+void ManagerPlanes::mostrarOpciones(){
+        cout << "MENU PRINCIPAL"<<endl;
+        cout << "====================="<< endl;
+        cout << "1 - PLANES"<< endl;
+        cout << "2 - EDITAR PRECIOS" << endl;
+        cout << "0 - SALIR"<< endl;
+        cout << "======================"<< endl;
+}
+
+int ManagerInstructores::seleccionOpcion(){
+    int opcion;
+    mostrarOpciones();
+    cout << "Opcion: ";
+    cin >> opcion;
+
+    while (opcion < 0 || opcion > _cantidadOpcines){
+        cout << "Opcion incorrecta..." << endl;
+        cout << "Opcion: ";
+        cin  >> opcion;
+    }
+    return opcion;
+
+}
+
+void ManagerPlanes::ejecutarOpcion(int opcion){
+    switch(opcion){
+        case 1:{
+        system("pause");
+        break;
+        }
+        case 2 : {
+        system("pause");
+        break;
+        }
+        case 0:{
+        break;
+        }
+    }
+}
---
+++ managerPlanes.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "plan.h"
+
+class ManagerPlanes{
+    public:
+    ManagerPlanes();
+    void run();
+
+    protected:
+    void mostrarOpciones();
+    int seleccionOpcion();
+    void ejecutarOpcion(int opcion);
+
+    private:
+    int _cantidadOpcines;
+    Plan Plan;
+
+};
