f713300e96cb2d3eb7d7cef2dcf49f0b01acfea9
Ignacio Rodriguez <132673620+nachorodrigguez@users.noreply.github.com>
2025-11-04 18:35:35 +0000

    Cobranza version 1

--- cobranza.cpp
+++ cobranza.cpp
@@ -1,1 +1,75 @@
+#include <iostream>
+#include <cstring>
+#include "cobranza.h"
 
+using namespace std;
+
+Cobranza::Cobranza(){
+    _numTransaccion = 0;
+    _descuentos = 0;
+    _montoTotal = 0;
+    strcpy(_metodoPago, "Indefinido");
+}
+
+int Cobranza::getNumTransaccion(){
+    return _numTransaccion;
+}
+Fecha Cobranza::getFechaTransaccion(){
+    return _fechaTransaccion;
+}
+float Cobranza::getDescuentos(){
+    return _descuentos;
+}
+float Cobranza::getMontoTotal(){
+    return _montoTotal;
+}
+const char* Cobranza::getMetodoPago(){
+    return _metodoPago;
+}
+
+void Cobranza::setNumTransaccion(int numTransaccion){
+    _numTransaccion = numTransaccion;
+}
+void Cobranza::setFechaTransaccion(Fecha fechaTransaccion){
+    _fechaTransaccion = fechaTransaccion;
+}
+void Cobranza::setDescuentos(float descuentos){
+    _descuentos = descuentos;
+
+    _montoTotal = _montoTotal - (_montoTotal *(_descuentos / 100));
+}
+void Cobranza::setMontoTotal(float montoTotal){
+    _montoTotal = montoTotal;
+}
+
+void Cobranza::Cargar(){
+        _numTransaccion++;
+        float descuentos;
+
+        cout << "Numero de transaccion: " << _numTransaccion << endl;
+        cout << "Fecha de transaccion: ";
+        _fechaTransaccion.Mostrar();
+
+        cout << endl;
+        cin.ignore();
+
+        cout << "Metodo de pago: ";
+        cin.getline(_metodoPago,50);
+
+        cout << "Descuentos: ";
+        cin >> descuentos;
+
+        setDescuentos(descuentos);
+
+        cout << "Monto total: " << _montoTotal << endl;
+}
+
+void Cobranza::Mostrar(){
+    cout << "Transaccion N°: " << _numTransaccion << endl;
+    cout << "Fecha: ";
+    _fechaTransaccion.Mostrar();
+
+    cout << "Descuento: " << _descuentos << "%" << endl;
+    cout << "Monto total: $" << _montoTotal << endl;
+    cout << "Metodo de pago: " << _metodoPago << endl;
+}
--- cobranza.h
+++ cobranza.h
@@ -1,2 +1,29 @@
 #pragma once
+#include <string>
+#include "fecha.h"
 
+class Cobranza{
+    private:
+    int _numTransaccion;
+    Fecha _fechaTransaccion;
+    float _descuentos;
+    float _montoTotal;
+    char _metodoPago[50];
+
+    public:
+    Cobranza();
+
+    int getNumTransaccion();
+    Fecha getFechaTransaccion();
+    float getDescuentos();
+    float getMontoTotal();
+    const char* getMetodoPago();
+
+    void setNumTransaccion(int);
+    void setFechaTransaccion(Fecha);
+    void setDescuentos(float);
+    void setMontoTotal(float);
+
+    void Cargar();
+    void Mostrar();
+};
