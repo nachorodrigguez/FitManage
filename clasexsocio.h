bb64d898e5a68379bc234d9b961d699b05b89c01
IgnaQuiroA <agustin.qp@hotmail.com.ar>
2025-11-10 15:00:21 -0300

    Update clasexsocio.h

--- clasexsocio.h
+++ clasexsocio.h
@@ -1,29 +1,31 @@
 #pragma once
-#include <string>
-#include "Fecha.h"
-#include "clase.h"
-#include "socio.h"
+#include "fecha.h"
 
 class ClasexSocio{
-    private:
-    int _IDClasexSocio;
-    Clase _IDClase;
-    Socio _idSocio;
+private:
+    int _IDClasexSocio;    // ID de la inscripcion (puede ser autonumerico desde el manager)
+    int _idSocio;          // ID o DNI del socio
+    int _IDClase;          // ID de la clase (1-7 segun Clase::Cargar)
     Fecha _fechaInscripcion;
+    bool _estado;          // true = inscripto / false = baja de la inscripcion
 
-    public:
+public:
     ClasexSocio();
 
-    int getIDClasexSocio();
-    Clase getIDClase();
-    Socio getId();
-    Fecha getFechaInscripcion();
+    // Getters
+    int getIDClasexSocio() const;
+    int getIdSocio() const;
+    int getIDClase() const;
+    Fecha getFechaInscripcion() const;
+    bool getEstado() const;
 
-    void setIDClasexSocios(int);
-    void setIDClase(Clase);
-    void setidSocio(Socio);
-    void setFechaInscripcion(Fecha);
+    // Setters
+    void setIDClasexSocio(int);
+    void setIdSocio(int);
+    void setIDClase(int);
+    void setFechaInscripcion(const Fecha &);
+    void setEstado(bool);
 
-    void Cargar();
-    void Mostrar();
-} ;
+    void Cargar();      // pide datos para una nueva inscripcion
+    void Mostrar() const; // muestra la inscripcion
+};
