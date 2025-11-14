30f8030fb39acede69f86e4622783ef375b85589
IgnaQuiroA <agustin.qp@hotmail.com.ar>
2025-11-14 10:18:52 -0300

    managersocios.h
    Se agrega lo relacionado con la eleccion de clase y plan

--- managersocios.h
+++ managersocios.h
@@ -17,5 +17,11 @@ class ManagerSocios{
         int _cantidadOpcines;
         Socio socio;
         ArchivoSocios archivoSocios;
+//----------------------------------------------------------------------
+        void asignarPlanOClase();  // submenú Asignar plan/clase
+        int  menuPlanes();         // 1) Plan 1, 2) Plan 2, 3) Plan 3, 0) Cancelar
+        int  pedirIdSocio();       // leer/validar ID socio
+        int  buscarPosSocioPorID(int idBuscado);
 
 };
+
