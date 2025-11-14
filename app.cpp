d47656228da79b44c6cbdc725f291c1372b38fef
Ignacio Rodriguez <nachorodrigguez@gmail.com>
2025-11-14 18:00:45 -0300

    Update app

--- app.cpp
+++ app.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "app.h"
+#include "rlutil.h"
 
 using namespace std;
 
@@ -11,7 +12,7 @@ App::App():
     managerclases("clases.dat"),
     managerrep("cobranzas.dat")
         {
-            _cantidadOpciones = 8 ;
+            _cantidadOpciones = 7 ;
         }
 
 
@@ -19,38 +20,59 @@ App::App():
 void App::run(){
     int opcion;
     do{
-        system("cls");
+        rlutil::cls();
         opcion = seleccionOpcion();
-        system("cls");
+        rlutil::cls();
         ejecutarOpcion(opcion);
     }while(opcion!=0);
 }
 
 void App::mostrarOpciones(){
-        cout << "MENU PRINCIPAL"<<endl;
-        cout << "====================="<< endl;
-        cout << "1 - SOCIOS"<< endl;
-        cout << "2 - INSTRUCTORES" << endl;
-        cout << "3 - PLANES"<< endl;
-        cout << "4 - CLASES"<< endl;
-        cout << "5 - PAGOS"<< endl;
-        cout << "6 - CLASES POR SOCIOS" << endl;
-        cout << "6 - REPORTES"<< endl;
-        cout << "7 - CONFIGURACIONES"<< endl;
-        cout << "0 - SALIR"<< endl;
-        cout << "======================"<< endl;
+    rlutil::setColor(rlutil::CYAN);
+    rlutil::locate(CENTER_X, 10);
+    cout << "FITMANAGE - SISTEMA DE GESTION DE GIMNASIO";
+
+    rlutil::resetColor();
+
+    rlutil::locate(CENTER_X, 14);
+    cout << "==============================";
+
+    rlutil::locate(CENTER_X, 15);
+    cout << "        MENU PRINCIPAL";
+
+    rlutil::locate(CENTER_X, 16);
+    cout << "==============================";
+
+    rlutil::setColor(rlutil::LIGHTGREEN);
+    rlutil::locate(CENTER_X, 18);  cout << "1 - SOCIOS";
+    rlutil::locate(CENTER_X, 19);  cout << "2 - INSTRUCTORES";
+    rlutil::locate(CENTER_X,20);  cout << "3 - PLANES";
+    rlutil::locate(CENTER_X,21);  cout << "4 - CLASES";
+    rlutil::locate(CENTER_X,22);  cout << "5 - PAGOS";
+    rlutil::locate(CENTER_X,23);  cout << "6 - REPORTES";
+    rlutil::locate(CENTER_X,24);  cout << "7 - CONFIGURACIONES";
+    rlutil::locate(CENTER_X,26);  cout << "0 - SALIR";
+
+    rlutil::resetColor();
+
+    rlutil::locate(CENTER_X,28);
+    cout << "Seleccione una opcion: ";
 }
 
-int App::seleccionOpcion(){
+int App::seleccionOpcion() {
     int opcion;
     mostrarOpciones();
-    cout << "Opcion: ";
     cin >> opcion;
 
     while (opcion < 0 || opcion > _cantidadOpciones){
-        cout << "Opcion incorrecta..." << endl;
-        cout << "Opcion: ";
-        cin  >> opcion;
+        rlutil::setColor(rlutil::RED);
+        rlutil::locate(CENTER_X,30);
+        cout << "Opcion incorrecta...   ";
+        rlutil::resetColor();
+
+        rlutil::locate(CENTER_X,28);
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
     }
     return opcion;
 }
@@ -60,32 +82,32 @@ void App::ejecutarOpcion(int opcion){
         case 1:{
         managersocios.run();
         break;
-        system("pause");
+        rlutil::anykey();
         }
         case 2:{
         managerinstructores.run();
         break;
-        system("pause");
+        rlutil::anykey();
         }
         case 3:{
         managerplanes.run();
         break;
-        system("pause");
+        rlutil::anykey();
         }
         case 4:{
         managerclases.run();
         break;
-        system("pause");
+        rlutil::anykey();
         }
         case 5:{
         managercob.run();
         break;
         }
         case 6:{
+        managerrep.run();
         break;
         }
         case 7:{
-        managerrep.run();
         break;
         }
     }
